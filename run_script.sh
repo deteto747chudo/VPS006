7c5a6c9eafe71363565314683a7520ca60d07802
denislav <127776832+deteto747chudo@users.noreply.github.com>
2024-09-07 21:57:57 +0300

    Add files via upload

---
+++ run_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 mainsetup.py & python3 mainsetup1.py & python3 mainsetup2.py & python3 mainsetup3.py & python3 mainsetup4.py & python3 mainsetup5.py & python3 mainsetup6.py & python3 mainsetup7.py & python3 mainsetup8.py & python3 mainsetup9.py
+echo "All Python scripts have been executed."
